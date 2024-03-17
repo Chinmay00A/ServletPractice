4960c440464c6aeb17899a07866e1fcbb31564c8
Chinmay00A <chinmaymahakul099@gmail.com>
2024-03-17 08:07:19 +0530

    First Servlet Program

---
+++ src/main/java/in/chin/WelcomeServlet.java
@@ -0,0 +1,37 @@
+package in.chin;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class WelcomeServlet
+ */
+@WebServlet("/WelcomeServlet")
+public class WelcomeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public WelcomeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @throws IOException 
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+   
+	public void doGet(HttpServletRequest req,HttpServletResponse res)throws ServletException, IOException {
+		String name=req.getParameter("name");
+		PrintWriter pw=res.getWriter();
+		pw.append(name + "Good Morning");
+	}
+}
